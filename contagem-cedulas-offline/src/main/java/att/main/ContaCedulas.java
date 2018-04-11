b1dfc335d3d2214366e296e3b31e4c8da6adc51f
Paulo Igor <piagodinho@gmail.com>
2018-04-11 08:57:45 -0300

    adicionando estrutura básica do projeto de contagem cedulas

---
+++ contagem-cedulas-offline/src/main/java/att/main/ContaCedulas.java
@@ -0,0 +1,5 @@
+package att.main;
+
+public class ContaCedulas {
+
+}
---
+++ contagem-cedulas-offline/src/test/java/att/main/ContaCedulasTest.java
@@ -0,0 +1,14 @@
+package att.main;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ContaCedulasTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
